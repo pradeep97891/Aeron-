4e580bc9ce0c68356c79c71a65fd6eab2ade9ae6
pradeep <pradeepkumar@infinitisoftware.net>
2025-06-12 12:16:37 +0530

    Initial commit with base pack

--- client/src/pages/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { FlightDashboard } from "@/components/flight-dashboard";
-
-export default function Dashboard() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <FlightDashboard />
-    </div>
-  );
-}
